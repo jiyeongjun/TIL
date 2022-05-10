096161490889fd2a6e1c625cdb42bcd60d11a557
JiYJ725 <wldudwns07@naver.com>
2022-05-10 21:06:55 +0900

    test

--- PractinceHTML/README.md
+++
@@ -1,2 +0,0 @@
-# HTML 연습
-기본 요소 및 Summit Form (유데미 강의 참조)
--- PractinceHTML/main.js
+++
@@ -1,8 +0,0 @@
-function isOldEnoughToDrink(age) {
-    // TODO: 여기에 코드를 작성합니다.
-    const result = age < 30 ? "여름이 매우 덥네요" : "에어컨을 켜야겠다!";
-    console.log(result);
-    return result;
-  }
-
-  isOldEnoughToDrink(30);
