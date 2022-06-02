7c8222d904b86c9de6d9690e9c2dfe4b55b586ad
JiYJ725 <wldudwns07@naver.com>
2022-06-02 09:34:35 +0900

    commit test

--- react-udemy/01-starting-setup/src/components/Expenses/Expenses.js
+++ react-udemy/01-starting-setup/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
+  // map활용
   const ExpenseItemList = props.items.map((el) => (
     <ExpenseItem title={el.title} amount={el.amount} date={el.date} />
   ));
