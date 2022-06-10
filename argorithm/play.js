71617e01777aa945f4962b8776a5d4025d32ac0a
JiYJ725 <wldudwns07@naver.com>
2022-06-10 16:43:11 +0900

    study: argorithm

--- argorithm/play.js
+++ argorithm/play.js
@@ -1,42 +1,17 @@
 const log = console.log;
 
-// bad
-function getUserType1(type) {
-  if (type === "ADMIN") {
-    return "관리자";
-  } else if (type === "INSTRUCTOR") {
-    return "강사";
-  } else if (type === "STUDENT") {
-    return "수강생";
-  } else {
-    return "해당 없음";
+function binarySearch(arr, val) {
+  // add whatever parameters you deem necessary - good luck!
+  let p1 = 0;
+  let p2 = arr.length - 1;
+  let res = Math.floor((p1 + p2) / 2);
+  while (arr[res] !== val) {
+    if (p1 > p2) return -1;
+    if (val < arr[res]) p2 = res - 1;
+    else p1 = res + 1;
+    res = Math.floor((p1 + p2) / 2);
   }
+  return res;
 }
 
-// good1 가장 바람직 하다.
-function getUserType2(type) {
-  const USER_TYPE = {
-    ADMIN: "관리자",
-    INSTRUCTOR: "강사",
-    STUDENT: "수강생",
-    UNDEFINED: "해당 없음",
-  };
-  return USER_TYPE[type] ?? USER_TYPE.UNDEFINED;
-}
-
-import USER_TYPE from "./constants/...some.js";
-function getUserType2(type) {
-  return USER_TYPE[type] ?? USER_TYPE.UNDEFINED;
-}
-
-// good2
-function getUserType3(type) {
-  return (
-    {
-      ADMIN: "관리자",
-      INSTRUCTOR: "강사",
-      STUDENT: "수강생",
-      UNDEFINED: "해당 없음",
-    }[type] ?? "해당 없음"
-  );
-}
+log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55, 90, 110], 1000));
