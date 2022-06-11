87ba15edaaf43ba3a452ac5ab3968fc6a4f11670
JiYJ725 <wldudwns07@naver.com>
2022-06-11 15:58:26 +0900

    study clean code

--- argorithm/play.js
+++ argorithm/play.js
@@ -1,15 +1,6 @@
 const log = console.log;
 
-function naiveSearch(long, short) {
-  let cnt = 0;
-  for (let i = 0; i < long.length; i++) {
-    for (let j = 0; j < short.length; j++) {
-      if (short[j] !== long[i + j]) break;
-      if (j === short.length - 1) cnt++;
-    }
-  }
-  return cnt;
+function changeValue(num) {
+  num++;
+  return num;
 }
-
-log(naiveSearch("lorie loled", "lol")); // 1
-log(naiveSearch("lorie loled", "lo")); // 1
