05852d0e63a3187513ee10ee9ead792925b5b478
JiYJ725 <wldudwns07@naver.com>
2022-06-14 22:55:35 +0900

    study : argorithm

---
+++ argorithm/backjun/10815.js
@@ -0,0 +1,5 @@
+// 백준 제출할 때 '/dev/stdin';
+
+const input = require("fs").readFileSync("backjun/input.txt").toString();
+console.log(process.cwd());
+console.log(input);
--- argorithm/play.js
+++ argorithm/play.js
@@ -1,6 +1,29 @@
 const log = console.log;
 
-function changeValue(num) {
-  num++;
-  return num;
-}
+// const solution = (p) => {
+//   const res = new Array(p.length).fill(0);
+
+//   for (let i = 0; i < p.length - 1; i++) {
+//     const min = Math.min(...p.slice(i));
+//     for (let j = i + 1; j < p.length; j++) {
+//       if (p[i] > p[j] && min === p[j]) {
+//         [p[i], p[j]] = [p[j], p[i]]; // p배열 위치 변경
+//         [res[i], res[j]] = [++res[i], ++res[j]]; // 자리 변경된 두 index에 1증가
+//       }
+//     }
+//   }
+//   return res;
+// };
+// log(solution(input1));
+// log(solution(input2));
+// log(solution(input3));
+
+const 화살표 = () => true;
+const 화살표2 = () => {
+  true;
+};
+const 화살표변수 = 화살표();
+const 화살표변수2 = 화살표2();
+
+console.log(화살표변수);
+console.log(화살표변수2);
