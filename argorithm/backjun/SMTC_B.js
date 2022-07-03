33e2080a6e645fd8018436064608511570da3d78
JiYJ725 <wldudwns07@naver.com>
2022-07-03 22:39:59 +0900

    sol : 원티드 대회 문제

---
+++ argorithm/backjun/SMTC_B.js
@@ -0,0 +1,7 @@
+//const readFileSyncAddress = "/dev/stdin"; // BackJun Submit
+const readFileSyncAddress = "backjun/input.txt"; // vscode Test
+const input = require("fs")
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trim()
+  .split("\n");
