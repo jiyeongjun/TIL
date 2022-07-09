46a457333daf609f4c33510d44efb15718c9de34
JiYJ725 <wldudwns07@naver.com>
2022-07-09 14:13:08 +0900

    jsdoc 기능 확인

--- TypeScript/typechain/src/myPackage.js
+++ TypeScript/typechain/src/myPackage.js
@@ -1,7 +1,7 @@
 // @ts-check
 /**
  * Initailizes the project
- * @param {object} config 
+ * @param {object} config
  * @param {boolean} config.debug
  * @param {string} config.url
  * @returns boolean
@@ -10,10 +10,34 @@ export function init(config) {
   return true;
 }
 /**
- * 
- * @param {number} code 
+ *
+ * @param {number} code
  * @returns number
  */
 export function exit(code) {
   return code + 1;
 }
+
+/**
+ * Gets the first element of `array`.
+ *
+ * @since 0.1.0
+ * @alias first
+ * @category Array
+ * @template T
+ * @param {Array<T>} array The array to query.
+ * @returns {*} Returns the first element of `array`.
+ * @see last
+ * @example
+ *
+ * head([1, 2, 3])
+ * // => 1
+ *
+ * head([])
+ * // => undefined
+ */
+function head(array) {
+  return array != null && array.length ? array[0] : undefined;
+}
+
+export default head;
