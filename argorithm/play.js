29ac5b7861e0a1b691422aebac93b713903774dc
JiYJ725 <wldudwns07@naver.com>
2022-07-20 22:20:20 +0900

    study argorithm

--- argorithm/play.js
+++ argorithm/play.js
@@ -6,3 +6,18 @@ const input = require("fs")
   .split("\n");
 
 const log = console.log;
+
+function solution(n, lost, reserve) {
+  const lostSet = new Set(lost.sort((a, b) => a - b));
+  const reserveSet = new Set(reserve.sort((a, b) => a - b));
+  for (const a of reserve) {
+    if (lostSet.has(a) === reserveSet.has(a)) {
+      lostSet.delete(a);
+      continue;
+    }
+    lostSet.delete(a - 1) || lostSet.delete(a + 1);
+  }
+  return n - lostSet.size;
+}
+
+log(solution(30, [1, 2, 6, 7], [1, 3, 5, 9]));
