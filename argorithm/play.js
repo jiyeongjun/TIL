f051a126af889fb8afe73bd3938df331e765cd34
JiYJ725 <wldudwns07@naver.com>
2022-07-25 13:31:44 +0900

    study algorithm

--- argorithm/play.js
+++ argorithm/play.js
@@ -1,23 +1,79 @@
-//const readFileSyncAddress = "/dev/stdin"; // BackJun Submit
-const readFileSyncAddress = "backjun/input.txt"; // vscode Test
+const readFileSyncAddress = "/dev/stdin"; // BackJun Submit
+//const readFileSyncAddress = "backjun/input.txt"; // vscode Test
+
 const input = require("fs")
   .readFileSync(readFileSyncAddress)
   .toString()
-  .split("\n");
+  .trim()
+  .split("\n")
+  .map(Number);
 
 const log = console.log;
 
-const getSpanStyle = (num) => {
-  let res = [];
-  for (let i; i < num; i++) {
-    res.push({ "--i": i });
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
   }
-  return res;
-};
 
-const spanStyle = getSpanStyle(16);
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      [this.values[parentIdx], this.values[idx]] = [element, parent];
+      idx = parentIdx;
+    }
+  }
 
-const testFunc = () => {
-  const test = 0;
-};
-log(spanStyle);
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    } else return 0;
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) leftChild = this.values[leftChildIdx];
+      if (leftChild > element) swap = leftChildIdx;
+
+      if (rightChildIdx < length) rightChild = this.values[rightChildIdx];
+      if (
+        (swap === null && rightChild > element) ||
+        (swap !== null && rightChild > leftChild)
+      )
+        swap = rightChildIdx;
+
+      if (swap === null) break;
+      [this.values[idx], this.values[swap]] = [this.values[swap], element];
+      idx = swap;
+    }
+  }
+}
+
+const heap = new MaxBinaryHeap();
+const res = [];
+
+for (const el of input.slice(1)) {
+  el || res.push(heap.extractMax());
+  heap.insert(el);
+}
+
+log(res.join("\n"));
