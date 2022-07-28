bd6a58e6e0f04740cf777d6bddadb8dc941a18e8
JiYJ725 <wldudwns07@naver.com>
2022-07-28 23:56:11 +0900

    study react reducer

--- argorithm/play.js
+++ argorithm/play.js
@@ -21,13 +21,26 @@ class Graph {
     }
   }
 
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
   addEdge(v1, v2) { // 간선추가
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
 
-  removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[]
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
   }
 }
 
@@ -37,5 +50,7 @@ g.addVertex("Tokyo");
 g.addVertex("Aspen");
 
 g.addEdge("Dallas", "Tokyo");
+
+g.removeEdge("Dallas", "Tokyo");
 console.log(g);
 
