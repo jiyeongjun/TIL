319f833f0b75f2ba3560f33ef0f41bd86ca3e59f
JiYJ725 <wldudwns07@naver.com>
2022-08-02 00:36:11 +0900

    study algorithm

--- argorithm/play.js
+++ argorithm/play.js
@@ -2,13 +2,18 @@ const log = console.log;
 //const readFileSyncAddress = "/dev/stdin"; // BackJun Submit
 const readFileSyncAddress = "backjun/input.txt"; // vscode Test
 
-const findCenter = (edges) => {
-  const temp = edges[0];
-  for (const a of edges.slice(1)) {
-    if (temp.includes(a[0])) return a[0];
-    if (temp.includes(a[1])) return a[1];
-  }
+
+const dispatch = (a) => {
+  if (typeof a === 'function') a(dispatch);
+  else console.log(a);
 }
 
-const edges = [[1, 2], [2, 3], [4, 2]];
-log(findCenter(edges));
+const returnFunc =
+  (message) =>
+    (dispatch) => {
+      dispatch({ a: 1 });
+      console.log(message);
+    }
+
+
+dispatch(returnFunc("메세지출력"));
